f872b4e8f4eef8fd33c636437d93cf4fa5f3ba9a
Krishna <krishnayad997@gmail.com>
2020-03-31 16:42:50 +0800

    made show route show username

--- server.py
+++ server.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 def index():
     return '<h1>Why so easy peasy</h1>'
 
-@app.route("/another")
-def show():
-    return '<h1>Yo</h1>'
+
+@app.route('/user/<username>')
+def show(username):
+    return f"Hi {username[0:]}"
 
 if __name__ == '__main__': # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
    app.run()
